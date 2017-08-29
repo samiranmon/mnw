0cd73ae7d6f1faf48f1ed206ac7e8c8313c7befd
samiranmon <samira.mmondal@gmail.com>
2017-08-29 11:48:02 +0530

    Show active user and cart count

--- test-extation/CCAvenue_MCPG_Magento_1.9.0.1/CCAvenue_Magento_MCPG_1.9.0.1_source/app/code/local/Mage/Ccavenuepay/Model/Config.php
+++ test-extation/CCAvenue_MCPG_Magento_1.9.0.1/CCAvenue_Magento_MCPG_1.9.0.1_source/app/code/local/Mage/Ccavenuepay/Model/Config.php
@@ -148,7 +148,8 @@ class Mage_Ccavenuepay_Model_Config
 	public function getCcavenuepayServerUrl()
 	{
 	   
-	     $url	= 'https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction';		  
+	     //$url	= 'https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction';		  
+	     $url	= 'https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction';		  
          return $url;
 	}
 	
